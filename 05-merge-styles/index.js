e7349a4d20ab20f667866301c2169a69e8299366
Nikita Sokov <nikitasokov1@gmail.com>
2025-01-22 11:35:48 +0300

    feat: complete 05-merge-styles task

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,22 @@
+const path = require("path");
+const pathSourceDir = path.join(__dirname, "styles");
+const pathSourceFile = path.join(__dirname, "project-dist", "bundle.css");
+const fs = require("fs/promises");
+
+(async function compileCssFile() {
+  try {
+    const files = await fs.readdir(pathSourceDir, { withFileTypes: true });
+
+    const cssFiles = files.filter((file) => {
+      return path.extname(file.name) === ".css";
+    });
+
+    const result = await Promise.all(cssFiles.map((file) => {
+      return fs.readFile(path.join(pathSourceDir, file.name));
+    }));
+
+    await fs.writeFile(pathSourceFile, result.join("\n"));
+  } catch (err) {
+    console.error(err);
+  }
+}) ();
